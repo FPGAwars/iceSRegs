67d5f1b826a44a2c6b66e39d46cd12c03c1c5569
Obijuan <obijuan.cube@gmail.com>
2023-11-03 09:38:24 +0100

    Sys-SR: Initial generation script

---
+++ Templates/generate-sys-sr.py
@@ -0,0 +1,17 @@
+from icestudio import generate_block_from_template
+
+BLOCK_ID = "Sys-SR"
+VERSION = "0.5"
+DESCRIPTION = "system shift right register. Verilog implementation"
+TARGET_PATH = f"../blocks/{BLOCK_ID}"
+
+#-- Debug: Generate only one block in the current path for testing
+generate_block_from_template(3,BLOCK_ID, VERSION, DESCRIPTION)
+
+#-- Generate all the blocks of different sizes (from 2 to 32 bits)
+#for i in range(2,33):
+#    generate_block_from_template(i, BLOCK_ID, VERSION, 
+#                                 DESCRIPTION,TARGET_PATH)  
+
+
+
