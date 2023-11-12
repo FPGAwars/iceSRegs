cd116fb813d2c5297fdcb865b48f5d5ce27c9911
Obijuan <obijuan.cube@gmail.com>
2023-11-12 12:27:42 +0100

    add 02-32-bits sys-sr-ld (Generated by the script)

--- Templates/generate-sys-sr-ld.py
+++ Templates/generate-sys-sr-ld.py
@@ -6,12 +6,12 @@ DESCRIPTION = "system shift right register with load input. Verilog implementati
 TARGET_PATH = f"../blocks/Right/{BLOCK_ID}"
 
 #-- Debug: Generate only one block in the current path for testing
-generate_block_from_template(3,BLOCK_ID, VERSION, DESCRIPTION)
+#generate_block_from_template(3,BLOCK_ID, VERSION, DESCRIPTION)
 
 #-- Generate all the blocks of different sizes (from 2 to 32 bits)
-#for i in range(2,33):
-#    generate_block_from_template(i, BLOCK_ID, VERSION, 
-#                                 DESCRIPTION,TARGET_PATH)  
+for i in range(2,33):
+    generate_block_from_template(i, BLOCK_ID, VERSION, 
+                                 DESCRIPTION,TARGET_PATH)  
 
 
 
