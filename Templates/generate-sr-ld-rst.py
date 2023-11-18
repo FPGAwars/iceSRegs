751c90c230d41b1e58bb4ed4ce0b8509eeb7e631
Obijuan <obijuan.cube@gmail.com>
2023-11-18 21:10:43 +0100

    sr-ld-rst: add template and script generator

---
+++ Templates/generate-sr-ld-rst.py
@@ -0,0 +1,16 @@
+from icestudio import generate_block_from_template
+
+BLOCK_ID = "SR-ld-rst"
+VERSION = "0.6"
+DESCRIPTION = "shift right register, with load and reset. "\
+              "Verilog implementation"
+TARGET_PATH = f"../blocks/Right/{BLOCK_ID}"
+
+#-- Debug: Generate only one block in the current path for testing
+#generate_block_from_template(3,BLOCK_ID, VERSION, DESCRIPTION)
+
+#-- Generate all the blocks of different sizes (from 2 to 32 bits)
+for i in range(2,33):
+    generate_block_from_template(i, BLOCK_ID, VERSION, 
+                                 DESCRIPTION,TARGET_PATH)  
+
