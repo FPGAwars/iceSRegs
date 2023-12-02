fcac9c7fcc48cf9ca39cd3828a695751e35d4db1
Obijuan <obijuan.cube@gmail.com>
2023-12-02 10:12:22 +0100

    update spanish translation (43%)

--- locale/es_ES/filtro.py
+++ locale/es_ES/filtro.py
@@ -116,7 +116,39 @@ msgstr "{bits:02}-SLR-ld-rst: Registro de desplazamiento izquierda-derecha "
     #-- Devolver la cadena
     return po
 
+#---------------------------
+#-- Cadena generica
+#---------------------------
+def test(po):
 
+    for bits in range(2,33):
+
+        #-- Cadena match
+        cad_match = f"""\
+msgid ""
+"{bits:02}-SLR-rst: {bits} bits shift left right register with reset. Verilog "
+"implementation"\
+"""
+
+
+        #-- Cadena fuente
+        cad_src = f"""\
+{cad_match}
+msgstr ""
+"""  
+        #-- Cadena destino
+        cad_target = f"""\
+{cad_match}
+msgstr "{bits:02}-SLR-ld-rst: Registro de desplazamiento izquierda-derecha "
+"con load y reset. Implementación en Verilog"
+"""
+        print(cad_match)
+
+        #-- Reemplazar la cadena
+        po = po.replace(cad_src, cad_target)
+
+    #-- Devolver la cadena
+    return po
 
 
 #---------------------------------------
@@ -137,10 +169,10 @@ with open("es_ES.po", "r") as f:
     #po = bits_id(po, "SR-ld")
     #po = bits_id(po, "SR-ld-rst")
 
-    po = bits_id(po, "Sys-SL")
-    po = bits_id(po, "Sys-SL-rst")
-    po = bits_id(po, "Sys-SL-ld")
-    po = bits_id(po, "Sys-SL-ld-rst")
+    #po = bits_id(po, "Sys-SL")
+    #po = bits_id(po, "Sys-SL-rst")
+    #po = bits_id(po, "Sys-SL-ld")
+    #po = bits_id(po, "Sys-SL-ld-rst")
 
     #po = bits_id(po, "SL")
     #po = bits_id(po, "SL-rst")
@@ -148,10 +180,10 @@ with open("es_ES.po", "r") as f:
     #po = bits_id(po, "SL-ld-rst")
 
 
-    po = bits_id(po, "Sys-SLR")
-    po = bits_id(po, "Sys-SLR-rst")
-    po = bits_id(po, "Sys-SLR-ld")
-    po = bits_id(po, "Sys-SLR-ld-rst")
+    #po = bits_id(po, "Sys-SLR")
+    #po = bits_id(po, "Sys-SLR-rst")
+    #po = bits_id(po, "Sys-SLR-ld")
+    #po = bits_id(po, "Sys-SLR-ld-rst")
 
     #po = bits_id(po, "SLR")
     #po = bits_id(po, "SLR-rst")
@@ -161,6 +193,8 @@ with open("es_ES.po", "r") as f:
     #po = bits_SLR_description(po)
     #po = bits_SLR_ld_description(po)
     #po = bits_SLR_ld_rst_description(po)
+
+    po = test(po)
 
 
 #-- Escribir el fichero destino
