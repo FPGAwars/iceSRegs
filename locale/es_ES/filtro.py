d9f2ab311af2580ff8a78b6cc306ff2a569176fc
Obijuan <obijuan.cube@gmail.com>
2023-12-02 08:36:06 +0100

    update spanish translation script

--- locale/es_ES/filtro.py
+++ locale/es_ES/filtro.py
@@ -1,45 +1,71 @@
+#-------------------------------------
+#-- Añadir cadenas: "02-SLR"
+#-------------------------------------
+def bits_SLR(po):
+    """Añadir cadenas SLR"""
+
+    #------ Añadir cadenas bits-SLR
+    for bits in range(2,33):
+
+        #-- Cadena fuente
+        cad_src = f"""\
+msgid "{bits}-SLR"
+msgstr ""
+"""  
+        #-- Cadena destino
+        cad_target = f"""\
+msgid "{bits}-SLR"
+msgstr "{bits}-SLR"
+"""
+        print(cad_target)
+
+        #-- Reemplazar la cadena
+        po = po.replace(cad_src, cad_target)
+
+        #-- Devolver la cadena
+        return po
+
+#-----------------------------------------
+#-- Añadir cadenas: "02-SLR: Descripcion"
+#-----------------------------------------
+def bits_SLR_description(po):
+    #----- Cadenas SLR-Descripcion
+    for bits in range(2,33):
+
+        #-- Cadena fuente
+        cad_src = f"""\
+msgid "{bits:02}-SLR: {bits} bits shift left right register. Verilog implementation"
+msgstr ""
+"""  
+        #-- Cadena destino
+        cad_target = f"""\
+msgid "{bits:02}-SLR: {bits} bits shift left right register. Verilog implementation"
+msgstr "{bits:02}-SLR: Registro de desplazamiento izquierda-derecha de 3 bits. "
+"Implementación en verilog"
+"""
+        print(cad_target)
+
+        #-- Reemplazar la cadena
+        po = po.replace(cad_src, cad_target)
+
+        #-- Devolver la cadena
+        return po
+
+
+#---------------------------------------
+#-- MAIN
+#---------------------------------------
+
 #-- Abrir el fichero con las traducciones
 with open("es_ES.po", "r") as f:
     po = f.read()
 
-
-#------ Añadir cadenas bits-SLR
-for bits in range(2,33):
-
-    #-- Cadena fuente
-    cad_src = f"""\
-msgid "{bits}-SLR"
-msgstr ""
-"""  
-    #-- Cadena destino
-    cad_target = f"""\
-msgid "{bits}-SLR"
-msgstr "{bits}-SLR"
-"""
-    print(cad_target)
-
-    #-- Reemplazar la cadena
-    po = po.replace(cad_src, cad_target)
+    #po = bits_SLR(po)
+    po = bits_SLR_description(po)
 
 
-#----- Cadenas SLR-Descripcion
-for bits in range(2,2):
 
-    #-- Cadena fuente
-    cad_src = f"""\
-msgid "{bits:02}-SLR: {bits} bits shift left right register. Verilog implementation"
-msgstr ""
-"""  
-    #-- Cadena destino
-    cad_target = f"""\
-msgid "{bits:02}-SLR: {bits} bits shift left right register. Verilog implementation"
-msgstr "{bits:02}-SLR: Registro de desplazamiento izquierda-derecha de 3 bits. Implementac"
-"ión en verilog""
-"""
-    print(cad_target)
 
-    #-- Reemplazar la cadena
-    po = po.replace(cad_src, cad_target)
 
 
 
