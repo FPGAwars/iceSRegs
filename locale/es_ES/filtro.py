0717bd9b4c6e861308e9cb95ab1c2b458aef998d
Obijuan <obijuan.cube@gmail.com>
2023-12-02 12:53:08 +0100

    update spanish translation (63%)

--- locale/es_ES/filtro.py
+++ locale/es_ES/filtro.py
@@ -127,7 +127,9 @@ def test(po):
 
         #-- Cadena match
         cad_match = f"""\
-msgid "{bits:02}-Sys-SL: {bits} bits system shift left register. Verilog implementation"\
+msgid ""
+"{bits:02}-Sys-SL-rst: {bits} bits system shift left register, with reset. Verilog "
+"implementation"\
 """
 
         #-- Cadena fuente
@@ -139,7 +141,7 @@ msgstr ""
         #-- Cadena destino
         cad_target = f"""\
 {cad_match}
-msgstr "{bits:02}-Sys-SL: Registro de desplazamiento a la izquierda de {bits} bits, del sistema. "
+msgstr "{bits:02}-Sys-SL-rst: Registro de desplazamiento a la izquierda de {bits} bits, del sistema, cn reset. "
 "Implementación en Verilog"
 """
         print(cad_match)
@@ -205,6 +207,8 @@ with open("es_ES.po", "r") as f:
     #SR-ld-rst
 
     #Sys-SL
+    #Sys-SL-rst (TODO)
+
     #SL
     #SL-rst
     #SL-ld
