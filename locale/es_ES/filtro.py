1cd3b392c1ba215bb24cff8b35d593884a10a191
Obijuan <obijuan.cube@gmail.com>
2023-12-02 08:47:58 +0100

    update spanish translation script

--- locale/es_ES/filtro.py
+++ locale/es_ES/filtro.py
@@ -1,29 +1,28 @@
 #-------------------------------------
-#-- Añadir cadenas: "02-SLR"
+#-- Añadir cadenas: "02-id"
 #-------------------------------------
-def bits_SLR(po):
-    """Añadir cadenas SLR"""
+def bits_id(po, id):
 
     #------ Añadir cadenas bits-SLR
     for bits in range(2,33):
 
         #-- Cadena fuente
         cad_src = f"""\
-msgid "{bits}-SLR"
+msgid "{bits:02}-{id}"
 msgstr ""
 """  
         #-- Cadena destino
         cad_target = f"""\
-msgid "{bits}-SLR"
-msgstr "{bits}-SLR"
+msgid "{bits:02}-{id}"
+msgstr "{bits:02}-{id}"
 """
         print(cad_target)
 
         #-- Reemplazar la cadena
         po = po.replace(cad_src, cad_target)
 
-        #-- Devolver la cadena
-        return po
+    #-- Devolver la cadena
+    return po
 
 #-----------------------------------------
 #-- Añadir cadenas: "02-SLR: Descripcion"
@@ -48,8 +47,8 @@ msgstr "{bits:02}-SLR: Registro de desplazamiento izquierda-derecha de 3 bits. "
         #-- Reemplazar la cadena
         po = po.replace(cad_src, cad_target)
 
-        #-- Devolver la cadena
-        return po
+    #-- Devolver la cadena
+    return po
 
 
 #---------------------------------------
@@ -60,8 +59,9 @@ msgstr "{bits:02}-SLR: Registro de desplazamiento izquierda-derecha de 3 bits. "
 with open("es_ES.po", "r") as f:
     po = f.read()
 
-    #po = bits_SLR(po)
+    po = bits_id(po, "SLR")
     po = bits_SLR_description(po)
+    #po = bits_id(po, "SLR-ld")
 
 
 
